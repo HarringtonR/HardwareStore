d5eb05d634764dc961e0f568da2c2ed43606d6a9
HarringtonR <ross.ludeke@gmail.com>
2019-02-15 14:46:21 -0600

    css

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,8 +13,8 @@ export default class Header extends Component {
             <ul>
               <a className='department transform' href='#section1'><li>Departments<img src='https://imgur.com/U9IsugV.png' alt='wrench' /></li></a>
               <a className='services transform' href=''><li>Services<img src='https://imgur.com/uKoJiOg.png' alt='key'/></li></a>
-              <a className='vendors transform' href='#section2'><li>Vendors<img src='https://imgur.com/SWtaqNf.png' alt='vendor' /></li></a>
-              <a className='history transform'href=''><li>History <img src='https://imgur.com/qryt6Ap.png' alt='info' /></li></a>
+              <a className='vendors transform' href='#section2'><li>Vendors<img src='https://imgur.com/wg1Tjpy.png' alt='vendor' /></li></a>
+              <a className='history transform'href=''><li>History <img src='https://imgur.com/k9IoVW6.png' alt='info' /></li></a>
               <a className='contact transform' href=''><li>Contact <img src='https://imgur.com/WgVpJwv.png' alt='mail' /></li> </a>
             </ul>
           </div>
