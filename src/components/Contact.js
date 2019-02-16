10fdda031d039dc3a5a80261828e4d54a8f3e7a8
HarringtonR <ross.ludeke@gmail.com>
2019-02-15 21:06:37 -0600

    contact maps begin

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,6 +7,16 @@ export default class Contact extends Component {
         <h4> Contact </h4>
         <hr />
           <div className='contactPage'>
+            <div className='maps'>
+            </div>
+            <div className='Inquiries'>
+              <h5>Inqueries</h5>
+              <p>For any inquiries, questions or commendations, please call: 123-456-7890 or fill out the following form</p>
+            </div>
+            <div>
+              <h5>Employement</h5>
+              <p>To apply for a job with Sphere Constuctions, please send a cover letter together with your C.V. to: info@mysite.com</p>
+            </div>
           </div>
       </div>
     );
--- src/components/History.js
+++ src/components/History.js
@@ -8,7 +8,7 @@ export default class History extends Component {
         <hr />
         <div className='historyWrap'>
           <div className='historyText'>
-            <p> Opened in 1945, <strong>Nicholson Hardware</strong> has been owned and operated by the Nicholson Family for three generations. A commitment to serving our customers, both contractor and homeowner/do-it-yourselfer, has remained our top priority. We strive to stock a good selection of hard-to-find items and have a dedicated sales staff with many years of experience. </p>
+            <p> Opened in 1945, Nicholson Hardware has been owned and operated by the Nicholson Family for three generations. A commitment to serving our customers, both contractor and homeowner/do-it-yourselfer, has remained our top priority. We strive to stock a good selection of hard-to-find items and have a dedicated sales staff with many years of experience. </p>
             <p> The Hardware Store is operated out of a 25,000 sq. ft. retail space located at 1131 2nd Avenue in Rockford’s Midtown District. The Lawn and Garden portion of the business was moved one block from the Hardware Store, to 1009 3rd Avenue, to accommodate the expansion of sales and service for both commercial and homeowner lawn and garden equipment. </p>
           </div>
           <div className='historyParrallax' />
