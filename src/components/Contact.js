f7a036f6b4a03a5dfb754d9517857a656b480067
HarringtonR <ross.ludeke@gmail.com>
2019-02-16 16:41:05 -0600

    working on maps

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import GoogleMaps from './GoogleMaps';
 
 export default class Contact extends Component {
   render() {
@@ -7,15 +8,22 @@ export default class Contact extends Component {
         <h4> Contact </h4>
         <hr />
           <div className='contactPage'>
-            <div className='maps'>
-            </div>
-            <div className='Inquiries'>
-              <h5>Inqueries</h5>
-              <p>For any inquiries, questions or commendations, please call: 123-456-7890 or fill out the following form</p>
-            </div>
-            <div>
-              <h5>Employement</h5>
-              <p>To apply for a job with Sphere Constuctions, please send a cover letter together with your C.V. to: info@mysite.com</p>
+            <GoogleMaps 
+            isMarkerShown
+              googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+  loadingElement={<div style={{ height: `100%` }} />}
+  containerElement={<div style={{ height: `400px` }} />}
+  mapElement={<div style={{ height: `100%` }} />}
+            />
+            <div className='contactInfo'>
+              <div className='Inquiries'>
+                <h5>Inqueries</h5>
+                <p>For any inquiries, questions or commendations, please call: 123-456-7890 or fill out the following form</p>
+              </div>
+              <div>
+                <h5>Employement</h5>
+                <p>To apply for a job with Sphere Constuctions, please send a cover letter together with your C.V. to: info@mysite.com</p>
+              </div>
             </div>
           </div>
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -15,7 +15,7 @@ export default class Header extends Component {
               <a className='services transform' href=''><li>Services<img src='https://imgur.com/uKoJiOg.png' alt='key'/></li></a>
               <a className='vendors transform' href='#section2'><li>Vendors<img src='https://imgur.com/wg1Tjpy.png' alt='vendor' /></li></a>
               <a className='history transform' href='#section4'><li>History <img src='https://imgur.com/k9IoVW6.png' alt='info' /></li></a>
-              <a className='contact transform' href=''><li>Contact <img src='https://imgur.com/WgVpJwv.png' alt='mail' /></li> </a>
+              <a className='contact transform' href='#section5'><li>Contact <img src='https://imgur.com/WgVpJwv.png' alt='mail' /></li> </a>
             </ul>
           </div>
     </Headroom>
